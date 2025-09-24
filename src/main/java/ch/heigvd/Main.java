20ce836b79249e86e3686d6016c15a244158f685
Schokiiiiiiii <dkcfab@gmail.com>
2025-09-24 17:30:48 +0200

    Files for commands and basic Main.java

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,17 +1,46 @@
+/*********************************************************************************************************************
+ * @filename    : Main.java
+ * @authors     : Fabien Léger and Samuel Dos Santos
+ * @version     : 0.1.0
+ * @updated on  : 24.09.2025
+ * @description : offers a CLI to interact with a CSV file and transform it.
+ *********************************************************************************************************************/
+
 package ch.heigvd;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import ch.heigvd.commands.CsvSort;
+import ch.heigvd.commands.CsvToMd;
+import picocli.CommandLine;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.File;
+
+@CommandLine.Command(
+        description = "A small program to modify and transform CSV files.",
+        version = "0.1.0",
+        showDefaultValues = true,
+        subcommands = {
+                CsvSort.class,
+                CsvToMd.class
+        },
+        scope = CommandLine.ScopeType.INHERIT,
+        mixinStandardHelpOptions = true)
+public class Main {
+
+    /**
+     * main that starts the right command following arguments given
+     * @param args parameters for picocli
+     */
+    public static void main(String[] args) {
+
+        // get filename
+        String jarFilename =
+                new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+                        .getName();
+
+        // do the command
+        int exitCode = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
+
+        // exit the program with exit code from command
+        System.exit(exitCode);
     }
 }
