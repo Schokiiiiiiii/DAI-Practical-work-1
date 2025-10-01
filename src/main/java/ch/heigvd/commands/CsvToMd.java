d7f4670ecb4d01472301085700bf6c7adf5225a0
Samurai-05 <samueldossant100@gmail.com>
2025-10-01 13:52:09 +0200

    Added main command layout

--- src/main/java/ch/heigvd/commands/CsvToMd.java
+++ src/main/java/ch/heigvd/commands/CsvToMd.java
@@ -1,4 +1,23 @@
 package ch.heigvd.commands;
 
-public class CsvToMd {
+import com.sun.tools.javac.Main;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "foo", description = "Convert a CSV file to a MARKDOWN table")
+public class CsvToMd implements Callable<Integer> {
+    @CommandLine.ParentCommand protected Main parent;
+
+    @Override
+    public Integer call() throws Exception{
+        System.out.println("Hello World!");
+        return 0;
+    }
+
+
+    private void convertCSVtoMD(){
+
+    }
 }
