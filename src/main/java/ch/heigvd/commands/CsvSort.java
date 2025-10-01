eaf9a6ce32dae135e5d6569d485bb49fb3e8b899
Schokiiiiiiii <dkcfab@gmail.com>
2025-10-01 13:52:54 +0200

    Base for CsvSort command

--- src/main/java/ch/heigvd/commands/CsvSort.java
+++ src/main/java/ch/heigvd/commands/CsvSort.java
@@ -1,4 +1,24 @@
 package ch.heigvd.commands;
 
-public class CsvSort {
+import ch.heigvd.Main;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "CsvSort", description = "Sorts a CSV file based on a column")
+public class CsvSort implements Callable<Integer> {
+
+    @CommandLine.ParentCommand protected Main parent;
+
+    @CommandLine.Option(
+            names = {"-c", "--column"},
+            description = "The header of the column to sort.",
+            defaultValue = "column1")
+    protected int column;
+
+    @Override
+    public Integer call() {
+        System.out.println("Here is the column: " + column);
+        return 0;
+    }
 }
