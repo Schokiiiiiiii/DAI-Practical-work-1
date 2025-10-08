15582d697fed1dce2bd10857119ea72ea0758fde
Schokiiiiiiii <dkcfab@gmail.com>
2025-10-08 16:14:39 +0200

    Fixed separator problem + added comments

--- src/main/java/ch/heigvd/commands/CsvSort.java
+++ src/main/java/ch/heigvd/commands/CsvSort.java
@@ -13,7 +13,7 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(name = "csvsort", description = "Sorts a CSV file based on a column")
 public class CsvSort implements Callable<Integer> {
 
-    private char SEPARATOR = ',';
+    private final char SEPARATOR = ',';
 
     @CommandLine.ParentCommand protected Main parent;
 
@@ -35,24 +35,28 @@ public class CsvSort implements Callable<Integer> {
     @Override
     public Integer call() {
 
+        // if no output filename -> same as input filename
         if (outputFilename == null) outputFilename = parent.getCSVFilename();
 
+        // search for column name's index
         int idx = findColumnIndex();
         if (idx == -1) {
             System.err.println("Error: Column not found!\n");
             return 1;
         }
 
+        // initialize data 2d array and retrieve the csv data
         ArrayList<ArrayList<String>> data = new ArrayList<>();
-
         retrieveData(data);
+
+        // TODO : remove show the variables for debugging
         for (ArrayList<String> datum : data) {
+            System.out.print('|');
             for (String s : datum) {
-                System.out.print(s);
+                System.out.print(s + '|');
             }
             System.out.println();
         }
-
         System.out.println("Here is the column : " + columnName);
         System.out.println("Here is the index  : " + idx);
 
@@ -67,25 +71,38 @@ public class CsvSort implements Callable<Integer> {
     // IOs that way and also eliminates huge file storage when column name is wrong
     private int findColumnIndex() {
 
+        // read from file
         try (Reader reader = new FileReader(parent.getCSVFilename());
              BufferedReader br = new BufferedReader(reader)) {
 
+            // initialize index to find
             int idx = 0;
 
+            // read the header line
             String line = br.readLine();
 
+            // loop over each separator
             int sep;
             while ((sep = line.indexOf(SEPARATOR)) != -1) {
 
+                // if there is a '"', fix the separator in case it was wrong
+                if (line.charAt(0) == '"') {
+                    sep = line.indexOf('"', 1);
+                    ++sep;
+                }
+
+                // take the word and check if it's the right one
                 String word = line.substring(0, sep);
                 if (word.equals(columnName))
                     return idx;
-                ++idx;
+
+                // start the line after the new separator
                 line = line.substring(sep + 1);
+                ++idx;
             }
 
-
-            if (line.equals(columnName)) return idx; // check last word
+            // check last word
+            if (line.equals(columnName)) return idx;
 
         } catch (IOException e) {
             System.out.println("Error with the csv input file: " + e.getMessage());
@@ -94,23 +111,43 @@ public class CsvSort implements Callable<Integer> {
         return 0;
     }
 
+    /**
+     * puts the data inside a 2d ArrayList given in parameter
+     * @param data ArrayList inside which to put the data
+     */
     private void retrieveData(ArrayList<ArrayList<String>> data) {
 
+        // read from file
         try (Reader reader = new FileReader(parent.getCSVFilename());
              BufferedReader br = new BufferedReader(reader)) {
 
+            // loop over each line
             String line;
             while ((line = br.readLine()) != null) {
+
+                // add a new empty line to our data
                 data.add(new ArrayList<>());
 
+                // loop over each separator
                 int sep;
                 while ((sep = line.indexOf(SEPARATOR)) != -1) {
 
+                    // if there is a '"', fix the separator in case it was wrong
+                    if (line.charAt(0) == '"') {
+                        sep = line.indexOf('"', 1);
+                        ++sep;
+                    }
+
+                    // get the word and add it to our data
                     String word = line.substring(0, sep);
                     data.getLast().add(word);
+
+                    // start the line after the separator
                     line = line.substring(sep + 1);
                 }
-                data.getLast().add(line); // add last word
+
+                // add last word
+                data.getLast().add(line);
             }
 
         } catch (IOException e) {
